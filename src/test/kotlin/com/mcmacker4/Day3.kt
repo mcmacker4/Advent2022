9d1a1721dd8015b58b6bc7aa6242a427626e13b4
Hans Geel <mcmacker4@gmail.com>
2023-01-19 23:24:16 +0100

    Day 3

--- src/test/kotlin/com/mcmacker4/Day3.kt
+++ src/test/kotlin/com/mcmacker4/Day3.kt
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test
 
 class Day3 : AdventDay(3) {
 
-    private val abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    private val score = abc.toCharArray().mapIndexed { i, c -> Pair(c, i + 1) }.toMap()
+    private val priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .toCharArray().mapIndexed { i, c -> Pair(c, i + 1) }.toMap()
 
     @Test
     fun part1() {
@@ -14,8 +13,23 @@ class Day3 : AdventDay(3) {
             .map { Pair(it.substring(0, it.length / 2), it.substring(it.length / 2)) }
             .map { Pair(it.first.toHashSet(), it.second.toHashSet()) }
             .map { it.first.intersect(it.second).first() }
-            .sumOf { score[it]!! }
+            .sumOf { priority[it]!! }
         log("Part 1: Priority $score")
+    }
+
+    @Test
+    fun part2() {
+        val lines = getInputLines()
+        val score = IntRange(0, lines.size - 1).zip(lines)
+            .groupBy({ it.first / 3 }, { it.second })
+            .values
+            .map(::findCommonItem)
+            .sumOf { priority[it]!! }
+        log("Part 2: Priority $score")
+    }
+
+    private fun findCommonItem(elves: List<String>): Char {
+        return elves.map { it.toCharArray().toSet() }.reduce(Set<Char>::intersect).first()
     }
 
 }
